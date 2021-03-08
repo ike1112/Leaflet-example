f4c565bb429ea1b9cea5ffb54dcfa0c410ebf21c
ike1112 <45696243+ike1112@users.noreply.github.com>
2021-03-07 21:39:29 -0700

    add react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+import LondonMap from "./LondonMap";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Use react-leaflet to create the london map</h1>
+      <LondonMap />
     </div>
   );
 }
